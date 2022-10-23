4ab58dcb53a56dae78dcc710e87926805340242f
Tebogo Nomnqa <tebzax2@gmail.com>
2022-10-23 21:23:07 +0200

    build: change name of project and add html meta data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,14 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <></>;
+  return (
+    <Head>
+      <title>Guess The Color</title>
+      <meta
+        name="description"
+        content="Guess the color react game, objective of identifying as much colors as you can in the given time"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
 }
