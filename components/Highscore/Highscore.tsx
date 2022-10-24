79a376a29afe57577a11002a0b4dfed592a580d8
Tebogo Nomnqa <tebzax2@gmail.com>
2022-10-24 16:24:52 +0200

    feat, build : add components to match the design, also add react-icons library

---
+++ components/Highscore/Highscore.tsx
@@ -0,0 +1,13 @@
+import { Heading } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+
+export default function HighScore() {
+  return (
+    <>
+      <Text fontSize="lg">High Score</Text>
+      <Heading size="lg" as="h1">
+        60
+      </Heading>
+    </>
+  );
+}
