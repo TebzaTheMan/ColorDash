0fbfde5b41cc4b347e2dbffbf4672bce364805fa
Tebogo Nomnqa <tebzax2@gmail.com>
2022-10-26 17:01:39 +0200

    feat: add cancel button on Inforbar for mobile

--- features/Infobar/components/Infobar.tsx
+++ features/Infobar/components/Infobar.tsx
@@ -1,20 +1,36 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { Score } from "./Score";
 import { Timer } from "./Timer";
 import { Tries } from "./Tries";
+import { Button } from "@chakra-ui/react";
+import Link from "next/link";
+import { useMediaQuery } from "@chakra-ui/react";
 
 export function Infobar() {
+  const [isMobile] = useMediaQuery("(max-width: 768px)");
   return (
-    <Flex
-      alignItems="center"
-      marginTop="8"
-      justifyContent={"space-between"}
-      marginLeft="8"
-      marginRight="8"
-    >
-      <Score />
-      <Timer />
-      <Tries />
-    </Flex>
+    <>
+      {isMobile && (
+        <Box marginTop="8" marginLeft="8">
+          <Link href="/" passHref>
+            <Button size="sm" variant="outline">
+              CANCEL
+            </Button>
+          </Link>
+        </Box>
+      )}
+
+      <Flex
+        alignItems="center"
+        marginTop="8"
+        justifyContent={"space-between"}
+        marginLeft="8"
+        marginRight="8"
+      >
+        <Score />
+        <Timer />
+        <Tries />
+      </Flex>
+    </>
   );
 }
