e5a45cc512733b9f752fb9aa4800ed060aa37b65
Tebogo Nomnqa <tebzax2@gmail.com>
2022-10-27 23:49:31 +0200

    feat:  generate an array of random colors and randomly choose one of them to be the color code to be guessed

--- features/colorblocks/components/Colorblock.tsx
+++ features/colorblocks/components/Colorblock.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import { GridItem } from "@chakra-ui/react";
-
-export function Colorblock() {
+interface Props {
+  color: string;
+  isCorrect: boolean;
+}
+export function Colorblock({ color, isCorrect }: Props) {
+  const [isClicked, setisClicked] = useState(false);
   return (
     <GridItem
       w="100%"
       h={["130", "178"]}
-      bg="blue.100"
+      bg={color}
       borderRadius="3xl"
       boxShadow="lg"
+      onClick={() => {
+        setisClicked(true);
+      }}
+      visibility={isClicked && !isCorrect ? "hidden" : "inherit"}
     />
   );
 }
--- features/colorblocks/components/Colorblocks.tsx
+++ features/colorblocks/components/Colorblocks.tsx
@@ -3,8 +3,29 @@ import { useMediaQuery } from "@chakra-ui/react";
 import Link from "next/dist/client/link";
 import { Colorblock } from "./Colorblock";
 
+const NUM_COLORS = 6;
+// generate a random rgb color value
+const getRgbColor = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  const value = `rgb(${red}, ${green}, ${blue})`;
+  return value;
+};
+// generaate an array of colors
+const getRGBcolors = () => {
+  const colors = [];
+  for (let i = 0; i < NUM_COLORS; i++) {
+    colors.push(getRgbColor());
+  }
+  return colors;
+};
+
 export function Colorblocks() {
   const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
+  const colors = getRGBcolors();
+  // assign correct color to one random color
+  const correctColor = colors[Math.floor(Math.random() * NUM_COLORS)];
   return (
     <Box>
       <Grid
@@ -23,7 +44,7 @@ export function Colorblocks() {
           </Box>
         )}
         <Heading size="lg" as="h1" color="gray.600" justifySelf={"center"}>
-          rgb(99, 143, 179)
+          {correctColor}
         </Heading>
       </Grid>
 
@@ -33,12 +54,15 @@ export function Colorblocks() {
         ml={["10", "32", "32", "72"]}
         mr={["10", "32", "32", "72"]}
       >
-        <Colorblock />
-        <Colorblock />
-        <Colorblock />
-        <Colorblock />
-        <Colorblock />
-        <Colorblock />
+        {colors.map((color, index) => {
+          return (
+            <Colorblock
+              color={color}
+              key={index}
+              isCorrect={color == correctColor ? true : false}
+            />
+          );
+        })}
       </Grid>
     </Box>
   );
