f0e9e85f2eaacb4147c8a723e61db4d7bc586747
Tebogo Nomnqa <tebzax2@gmail.com>
2022-12-05 16:03:18 +0200

    feat: add and setup Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { HighscoreProvider } from "features/Highscore";
 import { InfobarProvider } from "features/Infobar/contexts/Infobar.context";
@@ -8,6 +9,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <HighscoreProvider>
         <InfobarProvider>
           <Component {...pageProps} />
+          <Analytics />
         </InfobarProvider>
       </HighscoreProvider>
     </ChakraProvider>
