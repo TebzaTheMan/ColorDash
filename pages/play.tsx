43f44e4098c6db30453156a9dff5b1a4f2083ba2
Tebogo Nomnqa <tebzax2@gmail.com>
2022-12-08 17:07:25 +0200

    fix: document title does not say game over when it is gameover

--- pages/play.tsx
+++ pages/play.tsx
@@ -1,12 +1,16 @@
 import { Colorblocks } from "features/colorblocks";
-import { Infobar } from "features/Infobar";
+import { Infobar, InfobarContext } from "features/Infobar";
 import { GameoverModal } from "features/GameoverModal";
 import Head from "next/head";
+import { useContext } from "react";
 export default function Play() {
+  const [infobarData] = useContext(InfobarContext);
+  const isTimeUp = infobarData.timeUp;
+
   return (
     <>
       <Head>
-        <title>Guessing Colors Now</title>
+        <title>{isTimeUp ? "Game Over" : "Guessing Colors Now"}</title>
         <meta
           name="description"
           content="Guess the color react game, objective of identifying as much colors as you can in the given time"
